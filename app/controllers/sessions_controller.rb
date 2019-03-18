fbd863db782faa8b1685ec749d1ff0f1d525816b
jshli <joshua.li@mu-aa.com>
2019-03-18 16:14:12 +1100

    cors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,10 @@
 class SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def new
   end
 
   def create
+    
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
