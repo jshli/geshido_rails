0a65b79483621be11ec48ce47145dd9d14ca60ba
jshli <joshua.li@mu-aa.com>
2019-04-09 08:11:22 +1000

    prep for deploy

---
+++ app/javascript/components/DateIcon.js
@@ -0,0 +1,13 @@
+import React from "react"
+import styled from "styled-components"
+
+const Icon = styled.div`
+    display: flex;
+    text-align: center;
+    flex-direction: column;
+    justify-content: center;
+`
+
+export default function DateIcon(props){
+    
+}
